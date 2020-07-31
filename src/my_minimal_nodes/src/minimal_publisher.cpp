13f6aff26ffb2a12cced0fe838b91d3f534d3aa1
Anirudh Praveen <anirudhpraveen2000@gmail.com>
2020-07-31 14:48:44 +0530

    Changed minimal_publisher.cpp to consume less CPU time

--- src/my_minimal_nodes/src/minimal_publisher.cpp
+++ src/my_minimal_nodes/src/minimal_publisher.cpp
@@ -9,11 +9,16 @@ int main(int argc, char **argv) {
 
     std_msgs::Float64 input_float;
 
+    // set the sleep timer for 1Hz repetition rate (arg is in units of Hz)
+    ros::Rate naptime(1.0)
     input_float.data = 0.0;
 
     while (ros::ok()) {
         input_float.data = input_float.data + 0.001;
 
         my_publisher_object.publish(input_float);
+
+        // To achieve specific loop frequency
+        naptime.sleep();
     }
 }
